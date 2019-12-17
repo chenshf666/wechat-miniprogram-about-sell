798a2f18f99e31f9e856dffcf2448d1c9a84e48d
chenshf26 <Aa124578>
2019-12-17 10:19:57 +0800

    room修改
    聊天页面变成点击进去就会自动刷新，无需手动点击

--- miniprogram/pages/room/index.js
+++ miniprogram/pages/room/index.js
@@ -6,10 +6,8 @@ Page({
 
   data: {
     openid: '',
-    queryResult: '',
     result_array:[]
   },
-
   onLoad: function (options) {
     if (app.globalData.openid) {
       this.setData({
@@ -17,12 +15,19 @@ Page({
       })
     }
   },
+  onShow: function () {
+    // 页面出现在前台时执行
+    // 查询数据库找到对应房间id
+    this.onQuery()
+  },
+  //导航进聊天室
   toChat: function(event){
     app.globalData.roomid = event.currentTarget.dataset.roomid
     wx.navigateTo({
       url: '../chat/room',
     })
   },
+  //查询数据库集合openid2groupid里有没有关于房间id的记录，有的话说明已经存在和本人相关对话
   onQuery: function () {
     const db = wx.cloud.database()
     const _ = db.command
@@ -36,8 +41,7 @@ Page({
     ])).get({
       success: res => {
         this.setData({
-          result_array: res.data,
-          queryResult: JSON.stringify(res.data, null, 2)
+          result_array: res.data
         })
       },
       fail: err => {
