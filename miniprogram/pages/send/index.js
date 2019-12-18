be178f2f5593ad415a2d1d76c84a5721ff457020
chenshf26 <Aa124578>
2019-12-18 17:55:10 +0800

    这次更新实现了多张图片上传
    不过现在发送的数据库记录的url变成了列表，square前端没有做修改

--- miniprogram/pages/send/index.js
+++ miniprogram/pages/send/index.js
@@ -12,20 +12,8 @@ Page({
     input_value2:'内容',
     imageChoose: false,
     imageUrl:'',
-    filePaths:'',
-    files: [{
-      url: 'http://mmbiz.qpic.cn/mmbiz_png/VUIF3v9blLsicfV8ysC76e9fZzWgy8YJ2bQO58p43Lib8ncGXmuyibLY7O3hia8sWv25KCibQb7MbJW3Q7xibNzfRN7A/0',
-  }, {
-      loading: true
-  }, {
-      error: true
-  }]
-  },
-  onLoad :function(){
-    this.setData({
-      selectFile: this.selectFile.bind(this),
-      uplaodFile: this.uplaodFile.bind(this)
-  })
+    filePaths:[],//'选择的图片列表',
+    max_count: 9
   },
   test: function(){
     console.log(this.data.input_value)
@@ -43,7 +31,7 @@ Page({
   doChoose: function(){
     let thisFile = this
     wx.chooseImage({
-      count: 9,
+      count: this.data.max_count,
       sourceType: ['album', 'camera'],
       success: function (res) {
         const filePath = res.tempFilePaths[0]
@@ -51,7 +39,7 @@ Page({
         thisFile.setData({
           imageChoose:true,
           imageUrl:filePath,
-          filePaths:res.tempFilePaths
+          filePaths:[...(thisFile.data.filePaths),...(res.tempFilePaths)]
         })
       },
       fail: e => {
@@ -74,33 +62,34 @@ Page({
       })
       return
     }
-    let thisFile = this
-    const filePath = this.data.imageUrl
+
     // 上传图片
     wx.showLoading({
       title: '上传中',
     })
-    const cloudPath = 'square/' + Date.now() + filePath.match(/\.[^.]+?$/)[0]
-    wx.cloud.uploadFile({
-      cloudPath,
-      filePath,
-      success: res => {
-        thisFile.onAdd(this.data.input_value,this.data.input_value2,res.fileID,app.globalData.userInfo)
-        app.globalData.fileID = res.fileID
-        app.globalData.cloudPath = cloudPath
-        app.globalData.imagePath = filePath
-      },
-      fail: e => {
-        console.error('[上传文件] 失败：', e)
-        wx.showToast({
-          icon: 'none',
-          title: '上传失败',
-        })
-      },
-      complete: () => {
-        wx.hideLoading()
-      }
-    })
+    let thisFile = this
+    let sucess_count = 0
+    const files_num = this.data.filePaths.length
+    for(var i = 0; i < files_num; i++){
+      const filePath = this.data.filePaths[i]
+      const cloudPath = 'square/' + Date.now() + app.globalData.openid 
+                          + i + filePath.match(/\.[^.]+?$/)[0]
+      wx.cloud.uploadFile({
+        cloudPath,
+        filePath,
+        success: res => { ++sucess_count },
+        complete: () => {
+          if(sucess_count == files_num){
+            thisFile.onAdd(this.data.input_value,
+                           this.data.input_value2,
+                           this.data.filePaths,
+                           app.globalData.userInfo)
+          }
+          wx.hideLoading()
+        }
+      })
+    }
+    
   },
   onAdd: function (info,info2, url, userInfo) {
     console.log(Date.now())
@@ -132,43 +121,20 @@ Page({
       }
     })
   },
-    chooseImage: function (e) {
-        var that = this;
-        wx.chooseImage({
-            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-            success: function (res) {
-                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-                that.setData({
-                    files: that.data.files.concat(res.tempFilePaths)
-                });
-            }
-        })
-    },
-    previewImage: function(e){
-        wx.previewImage({
-            current: e.currentTarget.id, // 当前显示图片的http链接
-            urls: this.data.files // 需要预览的图片http链接列表
-        })
-    },
-    selectFile(files) {
-        console.log('files', files)
-        // 返回false可以阻止某次文件上传
-    },
-    uplaodFile(files) {
-        console.log('upload files', files)
-        // 文件上传的函数，返回一个promise
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                reject('some error')
-            }, 1000)
-        })
-    },
-    uploadError(e) {
-        console.log('upload error', e.detail)
-    },
-    uploadSuccess(e) {
-        console.log('upload success', e.detail)
-    }
+  previewImage: function previewImage(e) {
+    var index = e.currentTarget.dataset.index;
 
+    this.setData({
+        previewCurrent: index,
+        showPreview: true
+    });
+  },
+  deletePic : function(e){
+    var index = e.detail.index;
+    var files = this.data.filePaths;
+    var file = files.splice(index, 1);
+    this.setData({
+        filePaths: files
+    })
+  }
 })
